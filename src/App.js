3380c5e058341007ca2fa6d2a43a3b0e8086f7ef
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-04-06 08:23:24 +0530

    apollo client modification

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
 import React, { Component } from 'react';
-import {graphql,ApolloProvider} from 'react-apollo';
+import { graphql, ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+import { typeDefs } from './schema';
 import './App.css';
 
-const client=new ApolloClient({
-  link:new HttpLink(),
-  cache:new InMemoryCache()
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
+const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
+const client = new ApolloClient({
+  link: new HttpLink(mockNetworkInterface),
+  cache: new InMemoryCache()
 });
 
-const channelsListQuery=gql`
+const channelsListQuery = gql`
 query ChannelsListQuery{
   channels{
     id
     name
   }
 }`;
-const ChannelsList=({data:{loading,error,channels}})=>{
-  if(loading){
+
+
+const ChannelsList = ({ data: { loading, error, channels } }) => {
+  console.log('----------channels---------', channels);
+  if (loading) {
     return <p>Loading...</p>;
   }
-  if(error){
+  if (error) {
     return <p>{error.message}</p>;
   }
   return (
     <ul>
-      {channels.map(ch=><li key={ch.id}>{ch.name}</li>)}
-      </ul>
+      {channels.map(ch => <li key={ch.id}>{ch.name}</li>)}
+    </ul>
   );
 }
-const ChannelsListWithData=graphql(channelsListQuery)(ChannelsList);
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Instant Message</h1>
-        </header>
-        <div className="channels-list">
-        <ChannelsListWithData/>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Instant Message</h1>
+          </header>
+          <div className="channels-list">
+            <ChannelsListWithData />
+          </div>
         </div>
-      </div>
       </ApolloProvider>
     );
   }
